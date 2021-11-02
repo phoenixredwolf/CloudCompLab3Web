0c7c6e551dd55a40e346fb19c796e0a4773fd1e0
phoenixredwolf <b.vogtman@outlook.com>
2021-11-02 16:57:42 -0400

    added config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_m5XgQOd2R',
+        userPoolClientId: '5l63uuk21rprcjetd0qkj7mbcp',
+        region: 'us-east-2'
+    },
+    api: {
+        invodeUrl: ''
+    }
+};
